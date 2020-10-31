708c2edda80188d1864416e3e6cf50b6781339f8
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2020-10-31 16:21:49 +0900

    Update q3052.java

--- 1차원 배열/q3052.java
+++ 1차원 배열/q3052.java
@@ -26,3 +26,19 @@ class Main {
     sc.close();
   }
 }
+
+# 다른 풀이
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> count = new HashSet<Integer>();
+
+        for(int i = 0; i < 10; i++){
+           count.add(Integer.parseInt(br.readLine()) % 42);
+        }
+        System.out.println(count.size());
+    }
+}
